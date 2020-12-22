eecc593a5d2a22a44cb0b41bbc4bf94f0e36c766
moonlightL <jx8996@163.com>
2020-12-22 10:45:41 +0800

    修改dark模式下闪白问题

--- src/main/resources/templates/theme/default/source/js/main.js
+++ src/main/resources/templates/theme/default/source/js/main.js
@@ -1,8 +1,8 @@
 ;(function() {
-    let current_mode = "current_mode";
+    let CURRENT_MODE = "current_mode";
 
     let checkTheme = function() {
-        let mode = sessionStorage.getItem(current_mode);
+        let mode = sessionStorage.getItem(CURRENT_MODE);
         if (!mode) {
             let hour = (new Date()).getHours();
             mode = (hour >= 6 && hour < 18 ? "light" : "dark");
@@ -10,9 +10,9 @@
         $("html").attr("mode", mode);
 
         if (mode === "light") {
-            $("#modeBtn").html("<i class='fa fa-moon-o'></i>");
+            $(".mode-container").html("<div id='modeBtn' class='mode'><i class='fa fa-moon-o'></i></div>");
         } else {
-            $("#modeBtn").html("<i class='fa fa-sun-o'></i>");
+            $(".mode-container").html("<div id='modeBtn' class='mode'><i class='fa fa-sun-o'></i></div>");
         }
     };
 
@@ -20,7 +20,7 @@
         $("#modeBtn").on("click", function () {
             let $html = $("html");
             let mode = ($html.attr("mode") === "light" ? "dark" : "light");
-            sessionStorage.setItem(current_mode, mode);
+            sessionStorage.setItem(CURRENT_MODE, mode);
             $html.attr("mode", mode);
             if (mode === "light") {
                 $(this).html("<i class='fa fa-moon-o'></i>");
@@ -98,7 +98,7 @@
 
     let clickEffect = function() {
         let $container = $("#pageContainer");
-        $container.on("click",function(e) {
+        $container.on("click", function(e) {
             let $i = $('<b></b>').text('❤');
             let x = e.pageX, y = e.pageY;
             $i.css({
