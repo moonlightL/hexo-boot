b83b72e31da50c4a920dc25362c3a367e9933681
moonlightL <jx8996@163.com>
2020-12-24 10:18:47 +0800

    调整样式

--- src/main/java/com/light/hexo/common/component/interceptor/VisitInterceptor.java
+++ src/main/java/com/light/hexo/common/component/interceptor/VisitInterceptor.java
@@ -52,9 +52,7 @@ public class VisitInterceptor extends HandlerInterceptorAdapter {
             this.eventPublisher.emit(new VisitEvent(IpUtil.getIpAddr(request), BrowserUtil.getBrowserName(request)));
         }
 
-        if (log.isDebugEnabled()) {
-            request.setAttribute("time", System.currentTimeMillis());
-        }
+        request.setAttribute("time", System.currentTimeMillis());
 
         return true;
     }
@@ -62,10 +60,8 @@ public class VisitInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         super.afterCompletion(request, response, handler, ex);
-        if (log.isDebugEnabled()) {
-            long start = (long) request.getAttribute("time");
-            log.info("=========visit afterCompletion 请求 {}, 耗时 {} ms========", URLDecoder.decode(request.getRequestURI(), "UTF-8"), (System.currentTimeMillis() - start));
-        }
+        long start = (long) request.getAttribute("time");
+        log.info("=========visit afterCompletion 请求 {}, 耗时 {} ms========", URLDecoder.decode(request.getRequestURI(), "UTF-8"), (System.currentTimeMillis() - start));
     }
 
     private void print(HttpServletResponse response, String result) throws IOException {
