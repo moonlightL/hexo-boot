0665a7e77977f96d6fdcc484257da1586fa86765
moonlightL <jx8996@163.com>
2021-12-24 14:08:52 +0800

    修复

--- src/main/java/com/light/hexo/business/portal/web/controller/IndexAlbumController.java
+++ src/main/java/com/light/hexo/business/portal/web/controller/IndexAlbumController.java
@@ -5,9 +5,11 @@ import com.light.hexo.business.admin.model.Album;
 import com.light.hexo.business.portal.common.CommonController;
 import com.light.hexo.business.portal.model.HexoPageInfo;
 import com.light.hexo.common.util.ExceptionUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
@@ -23,6 +25,8 @@ import java.util.Map;
 public class IndexAlbumController extends CommonController {
 
     private static final Integer PAGE_SIZE = 12;
+
+    private static final String ALBUM_AUTH_PAGE = "portal/album/auth.html";
 
     /**
      * 专辑列表
@@ -44,15 +48,36 @@ public class IndexAlbumController extends CommonController {
      * @param resultMap
      * @return
      */
-    @GetMapping(value = {"albumDetail/{albumId}/", "albumDetail/{albumId}/{pageNum}"})
-    public String albumsDetail(@PathVariable Integer albumId, @PathVariable(value="pageNum", required = false) Integer pageNum, Map<String, Object> resultMap) {
+    @RequestMapping(value = {"albumDetail/{albumId}/", "albumDetail/{albumId}/{pageNum}"})
+    public String albumsDetail(
+            @PathVariable Integer albumId,
+            @PathVariable(value="pageNum", required = false) Integer pageNum,
+            String visitCode,
+            Map<String, Object> resultMap) {
         Album album = this.albumService.findById(albumId);
         if (album == null) {
             ExceptionUtil.throwExToPage(HexoExceptionEnum.ERROR_ALBUM_NOT_EXIST);
         }
 
-        resultMap.put("album", album);
         pageNum = pageNum == null ? 1 : pageNum;
+
+        if (StringUtils.isNotBlank(album.getVisitCode())) {
+            if (StringUtils.isBlank(visitCode)) {
+                resultMap.put("name", album.getName());
+                resultMap.put("link", "/albumDetail/" + albumId + "/" + pageNum);
+                return ALBUM_AUTH_PAGE;
+            }
+
+            if (!album.getVisitCode().equals(visitCode)) {
+                resultMap.put("name", album.getName());
+                resultMap.put("link", "/albumDetail/" + albumId + "/" + pageNum);
+                resultMap.put("errorMsg", "访问密码不正确");
+                return ALBUM_AUTH_PAGE;
+            }
+
+        }
+
+        resultMap.put("album", album);
         HexoPageInfo pageInfo = this.albumDetailService.pageAlbumDetailByIndex(albumId, pageNum, PAGE_SIZE);
         resultMap.put("pageInfo", pageInfo);
         resultMap.put("currentNav", this.navService.findByLink("/albums/"));
