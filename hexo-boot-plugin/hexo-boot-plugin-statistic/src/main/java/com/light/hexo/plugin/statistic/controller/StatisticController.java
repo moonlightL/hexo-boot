1eac19f6522b34deb049101f827af4e97c95a1bf
moonlightL <jx8996@163.com>
2022-04-28 13:00:49 +0800

    删除多余代码

--- hexo-boot-plugin/hexo-boot-plugin-statistic/src/main/java/com/light/hexo/plugin/statistic/controller/StatisticController.java
+++ hexo-boot-plugin/hexo-boot-plugin-statistic/src/main/java/com/light/hexo/plugin/statistic/controller/StatisticController.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.print.attribute.standard.Sides;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
