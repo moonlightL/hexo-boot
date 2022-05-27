0d6c1c1c7b4f7abdb773ceb044b320606a105c9d
moonlightL <jx8996@163.com>
2022-05-27 15:21:03 +0800

    调整

--- README.md
+++ README.md
@@ -324,5 +324,4 @@ curl -L -o ~/.hexo-boot/application.yml --create-dirs https://gitee.com/moonligh
 * 将 Druid 数据源替换为 HikariCP
 * 抽离系统信息代码，以插件形式封装
 * 抽离第三方对象存储代码，以插件形式封装
-* 新增随机拉取二次元图片功能
 * 修改部分页面 UI 交互
---
+++ hexo-boot-plugin/hexo-boot-plugin-server/src/main/java/com/light/hexo/plugin/server/mapper/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Author MoonlightL
+ * @ClassName: package-info
+ * @ProjectName hexo-boot
+ * @Description: Mapper 需继承 BaseMapper
+ * @DateTime 2022/5/26, 0026 19:03
+ */
+package com.light.hexo.plugin.server.mapper;
+
