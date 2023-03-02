0d8e0454d03f12ec5b4e895f99c21b88bf6d3833
moonlightL <jx8996@163.com>
2023-03-02 17:28:45 +0800

    调整

--- hexo-boot-mapper/src/main/java/com/light/hexo/mapper/model/extend/ThemeFileExtension.java
+++ hexo-boot-mapper/src/main/java/com/light/hexo/mapper/model/extend/ThemeFileExtension.java
@@ -48,7 +48,7 @@ public class ThemeFileExtension implements Serializable {
     /**
      * 归类 1：基础信息 2：页面配置 3：布局设置 4：评论设置 5：资源设置 6：其他设置
      */
-    private Integer tab = 1;
+    private Integer tab = 6;
 
     /**
      * 启动项目后是否同步到数据库
